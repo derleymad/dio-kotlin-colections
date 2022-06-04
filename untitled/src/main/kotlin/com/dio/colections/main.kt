9caf4f2c37f28ecdac5a5770f8a574a151836cf8
derleymad <70535956+derleymad@users.noreply.github.com>
2022-06-04 07:30:53 -0300

    adding list files

---
+++ untitled/src/main/kotlin/com/dio/colections/main.kt
@@ -0,0 +1,37 @@
+package com.dio.colections
+
+fun main(){
+    val values = IntArray(5)
+    values[0] = 5
+    values[1] = 4
+    values[2] = 3
+    values[3] = 2
+    values[4] = 1
+
+//    values.forEach{ //Pode ser usado sem descrever a variável, a padrão é it
+//        println(it)
+//    }
+//    values.forEach{ individual -> // Descrevendo a variável
+//        println(individual)
+//    }
+//    for (i in values.indices){ // Para cada indice
+//        println(i) // 0,1,2,3,4
+//    }
+//    values.sort()
+//    for (i in values){
+//        println(i)
+//    }
+//    var array = intArrayOf(1,2,3,4,5)
+//    array.forEach { println(it) }
+
+//    val array =  Array(2) {""} // Posso declaras já com as chaves o "tipo" string
+//    array[0] = "asd"
+    var array = arrayOf(1,2)
+    array.forEachIndexed{ indice, valor -> //For eacho index, da o valor e o index
+        array[indice] += 10
+        println(valor)
+    }
+    array.forEachIndexed{indice, valor -> //
+        println(valor)
+    }
+}
